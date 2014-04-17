d2dba2c279de3d5c183fd831e32b605ce99e8da2
Andrew Cordivari <andrewcord@optonline.net>
2014-04-17 16:33:24 -0400

    Cleaned up the pins scaffold, deleted json files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Pinteresting::Application.routes.draw do
+  resources :pins
+
   devise_for :users
-  root "pages#/home"
+  root "pages#home"
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
